13eac9dc142feca9e0a818db178f687cf6b033d6
Kazi Emran <kaziemran425@gmail.com>
2026-05-14 14:13:41 +0600

    lay

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -16,7 +16,7 @@
           <q-item-section avatar><q-icon name="analytics" /></q-item-section>
           <q-item-section>Dashboard</q-item-section>
         </q-item>
-
+        <!-- hr-management -->
         <q-expansion-item icon="people" label="HR Management">
           <q-item clickable to="/hrm/employees" class="q-ml-md">
             <q-item-section>Employees</q-item-section>
@@ -24,17 +24,38 @@
           <q-item clickable to="/hrm/attendance" class="q-ml-md">
             <q-item-section>Attendance</q-item-section>
           </q-item>
+          <q-item clickable to="/hrm/leaves" class="q-ml-md">
+            <q-item-section>Leaves</q-item-section>
+          </q-item>
+          <q-item clickable to="/hrm/payroll" class="q-ml-md">
+            <q-item-section>Payroll</q-item-section>
+          </q-item>
+          <q-item clickable to="/hrm/shifts" class="q-ml-md">
+            <q-item-section>Shifts</q-item-section>
+          </q-item>
         </q-expansion-item>
+
+        <!-- inventory -->
 
         <q-expansion-item icon="inventory" label="Inventory">
           <q-item clickable to="/inventory/stock-in" class="q-ml-md">
             <q-item-section>Stock In</q-item-section>
           </q-item>
+          <q-item clickable to="/inventory/reports" class="q-ml-md">
+            <q-item-section>Reports</q-item-section>
+          </q-item>
+          <q-item clickable to="/inventory/suppliers" class="q-ml-md">
+            <q-item-section>Suppliers</q-item-section>
+          </q-item>
         </q-expansion-item>
+        <!-- accounting -->
 
         <q-expansion-item icon="account_balance_wallet" label="Accounting">
           <q-item clickable to="/accounting/income" class="q-ml-md">
             <q-item-section>Income Report</q-item-section>
+          </q-item>
+          <q-item clickable to="/accounting/expenses" class="q-ml-md">
+            <q-item-section>Expenses Report</q-item-section>
           </q-item>
         </q-expansion-item>
 
@@ -52,11 +73,11 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
 export default {
   setup() {
-    const drawer = ref(false)
-    return { drawer }
-  }
-}
+    const drawer = ref(false);
+    return { drawer };
+  },
+};
 </script>
